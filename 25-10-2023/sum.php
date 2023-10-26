c46fcabf2733ff6f39fbbdb88d67e980e422083a
sinchana c b <sinchanasinchu8951@gamil.com>
2023-10-26 16:00:16 +0530

    first commit

---
+++ 25-10-2023/sum.php
@@ -0,0 +1,8 @@
+<?php
+$a=5;
+$b=5.0;
+$type=TRUE;
+$name="sinchu";
+var_dump ($type);
+echo "<br>";
+?>
---
+++ 26-10-2023/comments.php
@@ -0,0 +1,10 @@
+<?php
+$a="Technerds";
+$b="sinchana";
+echo("$a<br>");
+//echo"single line comment";
+print"$a<br>";
+echo $a."<br>";
+print"Technerds"."<br>".$a;
+echo"the value is<br>",$a,"name is<br> $b";
+?>
